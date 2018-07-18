3e638686b6d0709ef8b1b941e2f06e5e799955fa
Josh.Haggin <Josh.Haggin@kiewit.com>
2018-07-18 16:31:21 -0500

    Signed-off-by: Josh.Haggin <Josh.Haggin@kiewit.com>

---
+++ Settings.TempPoint.ps1
@@ -0,0 +1,13 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.145
+	 Created on:   	12/12/2017 11:19 PM
+	 Created by:   	Josh.Haggin
+	 Organization: 	
+	 Filename:     	Settings.ps1
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
---
+++ Settings.ps1
@@ -0,0 +1,15 @@
+﻿$bomgar = "\\kiewitplaza\ktg\Active\KSS\KSS_Toolkit\Bomgar"
+$ccleaner = "\\kiewitplaza\ktg\Active\KSS\KSS_Toolkit\Bomgar\CCleaner portable"
+$emsi = "\\kiewitplaza\ktg\Active\KSS\KSS_Toolkit\Bomgar\EMSI soft emergency kit"
+$unlocker = "\\kiewitplaza\ktg\Active\KSS\KSS_Toolkit\Bomgar\IObitUnlockerPortable"
+$revo = "\\kiewitplaza\ktg\Active\KSS\KSS_Toolkit\Bomgar\RevoUninstaller_Portable"
+$tweaking = "\\kiewitplaza\ktg\Active\KSS\KSS_Toolkit\Bomgar\Tweaking.com - Windows Repair"
+
+$combo = @()
+
+$combo += Get-ChildItem $bomgar -Include "*.exe" -Name
+$combo += Get-ChildItem $ccleaner -Include "*.exe" -Name
+$combo += Get-ChildItem $emsi -Include "*.exe" -Name
+$combo += Get-ChildItem $unlocker -Include "*.exe" -Name
+$combo += Get-ChildItem $revo -Include "*.exe" -Name
+$combo += Get-ChildItem $tweaking -Include "*.exe" -Name
